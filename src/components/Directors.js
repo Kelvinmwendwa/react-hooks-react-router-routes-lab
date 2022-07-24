5ccbc80800c23f12d2de15e97fcdd7779b51b6fc
Kelvin <kmwendwa476@gmail.com>
2022-07-24 23:00:34 +0300

    Completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <h1>Directors Page</h1>
+    {directors.map((director, index) => {
+      return <div key={index}>
+        <h3>Name: {director.name}</h3>
+        Movies: <ol>
+          {director.movies.map((movie, index)=>{
+            return <li key={index}>{movie}</li>
+          })}
+        </ol>
+      </div>
+    })}
+  </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <h2>Movies</h2>
+    { movies.map((movie, index) => {
+      return <div key={ index }>
+        <h2>Name: { movie.title }</h2>
+        <p>Time { movie.time }</p>
+        Genre : <ul>
+          { movie.genres.map((genre) => {
+            return <li key={ index }>{genre}</li>
+          })}
+        </ul>
+      </div>
+    })}
+  </div>;
 }
 
 export default Movies;
